679a8fa97d0c6809c214fc4f8193e51092e26288
Tiago Bras <tiagodsbras@gmail.com>
2016-04-26 16:59:12 +0100

    Add 'constants.py' file to 'tests'

--- activate.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-source ../venv/bin/activate
---
+++ tests/constants.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import os
+
+# Project Root directory
+ROOT_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
+# Tests directory
+TESTS_DIR = os.path.dirname(__file__)
+
+# Tests' data directory
+TESTS_DATA_DIR = os.path.join(TESTS_DIR, 'data')
